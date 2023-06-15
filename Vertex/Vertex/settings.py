8159c12a277bfe43197e4c9a48186009e702bd67
VardgesHambaryan <105721783+VardgesHambaryan@users.noreply.github.com>
2023-06-15 18:04:45 +0400

    Update settings.py

--- Vertex/Vertex/settings.py
+++ Vertex/Vertex/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--915%b2y9h5(6+b2-zqz%m_lfs(^8a@oc61b8bt58fxr#ivj=7'
+SECRET_KEY = config('SECRET KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
