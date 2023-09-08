d60585b4591282b9d914de84dc4389669e7b8320
VardgesHambaryan <105721783+VardgesHambaryan@users.noreply.github.com>
2023-09-08 20:53:03 +0400

    Update settings.py

--- Vertex/Vertex/settings.py
+++ Vertex/Vertex/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'haevolutionha@gmail.com'
-EMAIL_HOST_PASSWORD = 'ttiootenjwmbjshw'
+EMAIL_HOST_PASSWORD = 'YOUR ONE TIME PASSWORD'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
